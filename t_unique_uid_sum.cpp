8718e7588994a5d0471227d9c79c4f56dc9d9295
Devesh <d.devesh94@gmail.com>
2019-10-09 11:40:16 -0700

    Create t_unique_uid_sum.cpp

---
+++ t_unique_uid_sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int UniqueSum(vector<int> &arr) {
+  sort(arr.begin(), arr.end());
+  int l = arr.size();
+  for (int i = 1; i < l; i++) {
+    if (arr[i] == arr[i-1]) {
+      arr[i]++;
+    }
+  }
+
+  int sum = 0;
+  for (int i = 0; i < l; i++) {
+    cout << arr[i] << " ";
+    sum += arr[i];
+  }
+  cout << endl;
+
+  return sum;
+}
+
+int main () {
+  int n;
+  cin >> n;
+  vector<int> ids(n);
+  for (int i = 0; i < n; i++) {
+    cin >> ids[i];
+  }
+
+  cout << UniqueSum(ids) << endl;;
+}
